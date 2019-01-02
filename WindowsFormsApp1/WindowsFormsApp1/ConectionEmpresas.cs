d10a83666837652c912efee01009f5042f4d2553
adhair caceres huanca <adhaircC@outlook.com>
2019-01-01 22:11:48 -0500

    log in

---
+++ WindowsFormsApp1/WindowsFormsApp1/ConectionEmpresas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class ConectionEmpresas
+    {
+        SqlCommand cmd;
+        SqlDataReader reader;
+        SqlConnection SConection;
+
+        public void conectionEmpresas()
+        {
+            try
+            {
+                SConection = new SqlConnection("Data Source=DARKPEARL\\SQLEXPRESS01;Initial Catalog= moodCont;Integrated Security=true");
+                SConection.Open();
+                MessageBox.Show("conectado");
+            }
+            catch(Exception ex){
+                MessageBox.Show("no se conecto" + ex.ToString());
+            }
+        }
+        public string insert(string RUC, string Nombre, string anio, string mes,string direccion) 
+        {
+            string salida = "se inserto";
+            try
+            {
+                cmd = new SqlCommand("Insert into empresas(Ruc,Nombre,Date_Anio,Date_mes,Direccion) values('" + RUC + "','" + Nombre + "','" + anio + "','" + mes + "','" + direccion + "')",SConection);
+                cmd.ExecuteNonQuery();
+            } catch (Exception ex) {
+                salida="no se inserto" + ex.ToString();
+            }
+            return salida;
+        }
+        public int Validate(string RUC)
+        {
+            int existe = 0;
+            try
+            {
+                cmd = new SqlCommand("Select * from Empresas Where RUC="+ RUC+"'", SConection);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    existe++;
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("consulta fallida= " + ex.ToString());
+            }
+            return existe;
+        }
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -10,13 +10,24 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+ 
     public partial class Form1 : Form
     {
+        public partial class user
+        {
+            private string aUsuario;
+            private string aPassword;
+            public string usuario { get; set; }
+            public string passwrod { get; set; }
+        }
+        ConectionUsuarios conectarUser = new ConectionUsuarios();
+        ConectionEmpresas conectarSer = new ConectionEmpresas();
+        user usuario = new user();
         public Form1()
         {
+            conectarSer.conectionEmpresas();
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             if(textBox2.UseSystemPasswordChar == false)
@@ -31,15 +42,27 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // validar inicio de sección
-            inicio obj = new inicio();
-            obj.Visible = true;
-            Visible = false;
+            int validate=conectarUser.Validate(textBox1.Text, textBox2.Text);
+            if (validate == 1)
+            {
+                MessageBox.Show("usuario logeado");
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void id_user(object sender, EventArgs e)
+        {
+            usuario.usuario = sender.ToString();
+        }
+        private void password (object sender, EventArgs e)
+        {
+            usuario.passwrod = sender.ToString();
         }
     }
 }
