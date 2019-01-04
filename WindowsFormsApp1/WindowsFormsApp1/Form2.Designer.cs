13bd891b08077539c66303b8de2e6da25635073f
tonyccm2@outlook.es <tonyccm2@outlook.es>
2019-01-03 23:34:40 -0500

    asd

--- WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form2
+    partial class Mood
     {
         /// <summary>
         /// Required designer variable.
@@ -28,56 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelLateralIzq = new System.Windows.Forms.Panel();
             this.panelSubMenuAsientos = new System.Windows.Forms.Panel();
             this.buttonIngresarAsiento = new System.Windows.Forms.Button();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panelIngresarAsiento = new System.Windows.Forms.Panel();
-            this.panelIngresoDatosAsientos = new System.Windows.Forms.Panel();
-            this.buttonGenerarAsiento = new System.Windows.Forms.Button();
-            this.listBoxLibros_Asientos = new System.Windows.Forms.ListBox();
-            this.textBoxPeriodoMes_Asientos = new System.Windows.Forms.TextBox();
-            this.labelSucursal_Asientos = new System.Windows.Forms.Label();
-            this.labelPeriodo_Asientos = new System.Windows.Forms.Label();
-            this.textBoxSucursal_Asientos = new System.Windows.Forms.TextBox();
-            this.labelLibro_Asientos = new System.Windows.Forms.Label();
-            this.textBoxPeriodoAnio_Asientos = new System.Windows.Forms.TextBox();
             this.buttonAsientos = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
             this.buttonSubSistemas = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.buttonCuentas = new System.Windows.Forms.Button();
             this.buttonReportes = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.labelAsientos_Documento = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.panelLateralIzq.SuspendLayout();
             this.panelSubMenuAsientos.SuspendLayout();
-            this.panelIngresarAsiento.SuspendLayout();
-            this.panelIngresoDatosAsientos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelLateralIzq
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
-            this.panel1.Controls.Add(this.panelSubMenuAsientos);
-            this.panel1.Controls.Add(this.buttonAsientos);
-            this.panel1.Controls.Add(this.panel7);
-            this.panel1.Controls.Add(this.buttonSubSistemas);
-            this.panel1.Controls.Add(this.panel5);
-            this.panel1.Controls.Add(this.buttonCuentas);
-            this.panel1.Controls.Add(this.buttonReportes);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.panel4);
-            this.panel1.Controls.Add(this.pictureBoxLogo);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 461);
-            this.panel1.TabIndex = 0;
+            this.panelLateralIzq.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.panelLateralIzq.Controls.Add(this.panelSubMenuAsientos);
+            this.panelLateralIzq.Controls.Add(this.buttonAsientos);
+            this.panelLateralIzq.Controls.Add(this.panel7);
+            this.panelLateralIzq.Controls.Add(this.buttonSubSistemas);
+            this.panelLateralIzq.Controls.Add(this.panel5);
+            this.panelLateralIzq.Controls.Add(this.buttonCuentas);
+            this.panelLateralIzq.Controls.Add(this.buttonReportes);
+            this.panelLateralIzq.Controls.Add(this.panel3);
+            this.panelLateralIzq.Controls.Add(this.panel4);
+            this.panelLateralIzq.Controls.Add(this.pictureBoxLogo);
+            this.panelLateralIzq.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLateralIzq.Location = new System.Drawing.Point(0, 0);
+            this.panelLateralIzq.Name = "panelLateralIzq";
+            this.panelLateralIzq.Size = new System.Drawing.Size(200, 461);
+            this.panelLateralIzq.TabIndex = 0;
             // 
             // panelSubMenuAsientos
             // 
@@ -99,138 +84,6 @@
             this.buttonIngresarAsiento.TabIndex = 0;
             this.buttonIngresarAsiento.Text = "Ingresar Asiento";
             this.buttonIngresarAsiento.UseVisualStyleBackColor = true;
-            this.buttonIngresarAsiento.Click += new System.EventHandler(this.buttonIngresarAsiento_Click);
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.Color.Gray;
-            this.panel7.Location = new System.Drawing.Point(3, 244);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(5, 40);
-            this.panel7.TabIndex = 6;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.Gray;
-            this.panel5.Location = new System.Drawing.Point(3, 198);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(5, 40);
-            this.panel5.TabIndex = 5;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.Gray;
-            this.panel3.Location = new System.Drawing.Point(3, 152);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(5, 40);
-            this.panel3.TabIndex = 1;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.Gray;
-            this.panel4.Location = new System.Drawing.Point(3, 106);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(5, 40);
-            this.panel4.TabIndex = 4;
-            // 
-            // panelIngresarAsiento
-            // 
-            this.panelIngresarAsiento.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.f38bfb47e0d4bf986a6e92d86ec254f2;
-            this.panelIngresarAsiento.Controls.Add(this.panel2);
-            this.panelIngresarAsiento.Controls.Add(this.panelIngresoDatosAsientos);
-            this.panelIngresarAsiento.Location = new System.Drawing.Point(200, 0);
-            this.panelIngresarAsiento.Name = "panelIngresarAsiento";
-            this.panelIngresarAsiento.Size = new System.Drawing.Size(672, 461);
-            this.panelIngresarAsiento.TabIndex = 2;
-            this.panelIngresarAsiento.Visible = false;
-            // 
-            // panelIngresoDatosAsientos
-            // 
-            this.panelIngresoDatosAsientos.BackColor = System.Drawing.Color.Transparent;
-            this.panelIngresoDatosAsientos.Controls.Add(this.buttonGenerarAsiento);
-            this.panelIngresoDatosAsientos.Controls.Add(this.listBoxLibros_Asientos);
-            this.panelIngresoDatosAsientos.Controls.Add(this.textBoxPeriodoMes_Asientos);
-            this.panelIngresoDatosAsientos.Controls.Add(this.labelSucursal_Asientos);
-            this.panelIngresoDatosAsientos.Controls.Add(this.labelPeriodo_Asientos);
-            this.panelIngresoDatosAsientos.Controls.Add(this.textBoxSucursal_Asientos);
-            this.panelIngresoDatosAsientos.Controls.Add(this.labelLibro_Asientos);
-            this.panelIngresoDatosAsientos.Controls.Add(this.textBoxPeriodoAnio_Asientos);
-            this.panelIngresoDatosAsientos.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelIngresoDatosAsientos.Location = new System.Drawing.Point(0, 0);
-            this.panelIngresoDatosAsientos.Name = "panelIngresoDatosAsientos";
-            this.panelIngresoDatosAsientos.Size = new System.Drawing.Size(672, 29);
-            this.panelIngresoDatosAsientos.TabIndex = 7;
-            // 
-            // buttonGenerarAsiento
-            // 
-            this.buttonGenerarAsiento.Location = new System.Drawing.Point(597, 3);
-            this.buttonGenerarAsiento.Name = "buttonGenerarAsiento";
-            this.buttonGenerarAsiento.Size = new System.Drawing.Size(75, 21);
-            this.buttonGenerarAsiento.TabIndex = 7;
-            this.buttonGenerarAsiento.Text = "Siguiente";
-            this.buttonGenerarAsiento.UseVisualStyleBackColor = true;
-            // 
-            // listBoxLibros_Asientos
-            // 
-            this.listBoxLibros_Asientos.FormattingEnabled = true;
-            this.listBoxLibros_Asientos.Items.AddRange(new object[] {
-            "Libro Diario",
-            "Libro Mayor",
-            "Libro de Compras",
-            "Libro de Ventas e Ingresos",
-            "Libro Diario formato simplificado"});
-            this.listBoxLibros_Asientos.Location = new System.Drawing.Point(141, 6);
-            this.listBoxLibros_Asientos.Name = "listBoxLibros_Asientos";
-            this.listBoxLibros_Asientos.Size = new System.Drawing.Size(197, 17);
-            this.listBoxLibros_Asientos.TabIndex = 4;
-            // 
-            // textBoxPeriodoMes_Asientos
-            // 
-            this.textBoxPeriodoMes_Asientos.Location = new System.Drawing.Point(445, 3);
-            this.textBoxPeriodoMes_Asientos.Name = "textBoxPeriodoMes_Asientos";
-            this.textBoxPeriodoMes_Asientos.Size = new System.Drawing.Size(46, 20);
-            this.textBoxPeriodoMes_Asientos.TabIndex = 6;
-            // 
-            // labelSucursal_Asientos
-            // 
-            this.labelSucursal_Asientos.AutoSize = true;
-            this.labelSucursal_Asientos.Location = new System.Drawing.Point(7, 10);
-            this.labelSucursal_Asientos.Name = "labelSucursal_Asientos";
-            this.labelSucursal_Asientos.Size = new System.Drawing.Size(46, 13);
-            this.labelSucursal_Asientos.TabIndex = 0;
-            this.labelSucursal_Asientos.Text = "sucursal";
-            // 
-            // labelPeriodo_Asientos
-            // 
-            this.labelPeriodo_Asientos.AutoSize = true;
-            this.labelPeriodo_Asientos.Location = new System.Drawing.Point(344, 10);
-            this.labelPeriodo_Asientos.Name = "labelPeriodo_Asientos";
-            this.labelPeriodo_Asientos.Size = new System.Drawing.Size(43, 13);
-            this.labelPeriodo_Asientos.TabIndex = 5;
-            this.labelPeriodo_Asientos.Text = "Periodo";
-            // 
-            // textBoxSucursal_Asientos
-            // 
-            this.textBoxSucursal_Asientos.Location = new System.Drawing.Point(60, 3);
-            this.textBoxSucursal_Asientos.Name = "textBoxSucursal_Asientos";
-            this.textBoxSucursal_Asientos.Size = new System.Drawing.Size(39, 20);
-            this.textBoxSucursal_Asientos.TabIndex = 1;
-            // 
-            // labelLibro_Asientos
-            // 
-            this.labelLibro_Asientos.AutoSize = true;
-            this.labelLibro_Asientos.Location = new System.Drawing.Point(105, 10);
-            this.labelLibro_Asientos.Name = "labelLibro_Asientos";
-            this.labelLibro_Asientos.Size = new System.Drawing.Size(30, 13);
-            this.labelLibro_Asientos.TabIndex = 2;
-            this.labelLibro_Asientos.Text = "Libro";
-            // 
-            // textBoxPeriodoAnio_Asientos
-            // 
-            this.textBoxPeriodoAnio_Asientos.Location = new System.Drawing.Point(393, 4);
-            this.textBoxPeriodoAnio_Asientos.Name = "textBoxPeriodoAnio_Asientos";
-            this.textBoxPeriodoAnio_Asientos.Size = new System.Drawing.Size(46, 20);
-            this.textBoxPeriodoAnio_Asientos.TabIndex = 3;
             // 
             // buttonAsientos
             // 
@@ -248,6 +101,14 @@
             this.buttonAsientos.UseVisualStyleBackColor = true;
             this.buttonAsientos.Click += new System.EventHandler(this.button4_Click);
             // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Gray;
+            this.panel7.Location = new System.Drawing.Point(3, 244);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(5, 40);
+            this.panel7.TabIndex = 6;
+            // 
             // buttonSubSistemas
             // 
             this.buttonSubSistemas.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
@@ -262,6 +123,14 @@
             this.buttonSubSistemas.TabIndex = 6;
             this.buttonSubSistemas.Text = "Sub Sistemas";
             this.buttonSubSistemas.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Gray;
+            this.panel5.Location = new System.Drawing.Point(3, 198);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(5, 40);
+            this.panel5.TabIndex = 5;
             // 
             // buttonCuentas
             // 
@@ -293,6 +162,22 @@
             this.buttonReportes.Text = "Reportes";
             this.buttonReportes.UseVisualStyleBackColor = true;
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Location = new System.Drawing.Point(3, 152);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(5, 40);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Location = new System.Drawing.Point(3, 106);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(5, 40);
+            this.panel4.TabIndex = 4;
+            // 
             // pictureBoxLogo
             // 
             this.pictureBoxLogo.Image = global::WindowsFormsApp1.Properties.Resources.mood;
@@ -303,43 +188,18 @@
             this.pictureBoxLogo.TabIndex = 0;
             this.pictureBoxLogo.TabStop = false;
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.Controls.Add(this.labelAsientos_Documento);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 29);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(672, 100);
-            this.panel2.TabIndex = 8;
-            // 
-            // labelAsientos_Documento
-            // 
-            this.labelAsientos_Documento.AutoSize = true;
-            this.labelAsientos_Documento.Location = new System.Drawing.Point(7, 4);
-            this.labelAsientos_Documento.Name = "labelAsientos_Documento";
-            this.labelAsientos_Documento.Size = new System.Drawing.Size(62, 13);
-            this.labelAsientos_Documento.TabIndex = 0;
-            this.labelAsientos_Documento.Text = "Documento";
-            // 
-            // Form2
+            // Mood
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
             this.ClientSize = new System.Drawing.Size(884, 461);
-            this.Controls.Add(this.panelIngresarAsiento);
-            this.Controls.Add(this.panel1);
-            this.Name = "Form2";
+            this.Controls.Add(this.panelLateralIzq);
+            this.Name = "Mood";
             this.Text = "Form2";
-            this.panel1.ResumeLayout(false);
+            this.panelLateralIzq.ResumeLayout(false);
             this.panelSubMenuAsientos.ResumeLayout(false);
-            this.panelIngresarAsiento.ResumeLayout(false);
-            this.panelIngresoDatosAsientos.ResumeLayout(false);
-            this.panelIngresoDatosAsientos.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -354,20 +214,8 @@
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button buttonAsientos;
         private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelLateralIzq;
         private System.Windows.Forms.Panel panelSubMenuAsientos;
         private System.Windows.Forms.Button buttonIngresarAsiento;
-        private System.Windows.Forms.Panel panelIngresarAsiento;
-        private System.Windows.Forms.Panel panelIngresoDatosAsientos;
-        private System.Windows.Forms.ListBox listBoxLibros_Asientos;
-        private System.Windows.Forms.TextBox textBoxPeriodoMes_Asientos;
-        private System.Windows.Forms.Label labelSucursal_Asientos;
-        private System.Windows.Forms.Label labelPeriodo_Asientos;
-        private System.Windows.Forms.TextBox textBoxSucursal_Asientos;
-        private System.Windows.Forms.Label labelLibro_Asientos;
-        private System.Windows.Forms.TextBox textBoxPeriodoAnio_Asientos;
-        private System.Windows.Forms.Button buttonGenerarAsiento;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label labelAsientos_Documento;
     }
 }
