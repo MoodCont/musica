4ec4b8026705c436db25f6e27f72b9e5468700b4
tonyccm2@outlook.es <tonyccm2@outlook.es>
2019-01-08 00:38:36 -0500

    mood2

--- WindowsFormsApp1/WindowsFormsApp1/inicio.cs
+++ WindowsFormsApp1/WindowsFormsApp1/inicio.cs
@@ -87,5 +87,10 @@ namespace WindowsFormsApp1
             obj.Visible = true;
             Visible = false;
         }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
